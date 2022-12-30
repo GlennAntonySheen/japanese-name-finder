83df0cc9e3660e788978fc58523220ebab18551f
glenn <glennantonysheen@gmail.com>
2022-12-30 22:56:17 +0530

    :art: added backgroung image and improved UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,76 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import BackgroundImg from './assets/backgroundImg.svg'
 
 const PageWrapper = styled.div`
 	height: 100vh;
 	width: 100vw;
-  	background-color: orange;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+  	background-color: white;
+
+	img { 
+		height: 100vh;
+		width: 100vw;
+		position: absolute;
+	}
+`;
+
+const InputContainer = styled.div`
+	height: fit-content;
+	width: 45%;
+	padding: 1rem;
+	display: flex;
+	flex-direction: column;
+	/* justify-content: centre; */
+	align-items: center;
+	gap: 3rem;
+	z-index: 1;
+	/* background-color: green; */
+
+	input {
+		height: 150px;
+		width: 90%;
+		padding: 1rem;
+		flex: 2;
+		font-size: 30px;
+		border-radius: 1rem;
+		border: none;
+		outline: none;
+		box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+		background: rgba(255, 255, 255, 0.28);
+		border-radius: 16px;
+	}
+
+	h1 {
+		height: 150px;
+		width: 90%;
+		margin: 0;
+		padding: 1rem;
+		flex: 2; 
+		display: flex;
+		align-items: center;
+		border-radius: 1rem;
+		/* background-color: yellow; */
+	}
 `;
 
 export default function App() {
 	const [name, setName] = useState('')
+
+	function capitalizeFirstLetterOfEveryWord(string) {
+		if (string === '') {
+			return ''
+		}
+		const words = string.split(" ");
+
+		for (let i = 0; i < words.length; i++) {
+			words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+		}
+
+		return words.join(" ");
+	}
 
 	const findJapaneseName = (englishName) => {
 		englishName = englishName.toUpperCase()
@@ -45,16 +107,25 @@ export default function App() {
 		let finalName = ''
 
 		for (let i = 0; i < englishName.length; i++) {
-			finalName += JapaneseName[englishName[i]]
+			if (JapaneseName[englishName[i]] === undefined) {
+				finalName += englishName[i]
+			} else {
+				finalName += JapaneseName[englishName[i]]
+			}
+
 		}
 
-		setName(finalName)
+		setName(capitalizeFirstLetterOfEveryWord(finalName))
 	}
 
 	return <PageWrapper >
-		<input onChange={(event) => {
-			findJapaneseName(event.target.value)
-		}}></input>
-		<h1>{name}</h1>
+		<img src={BackgroundImg} alt='background image' />
+		{/* <BackgroundImg /> */}
+		<InputContainer>
+			<input
+				placeholder='Please, Enter Your Name'
+				onChange={(event) => findJapaneseName(event.target.value)} />
+			<h1>{name}</h1>
+		</InputContainer>
 	</PageWrapper>
 }
